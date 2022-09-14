486d142b189f41b1454d66453bac3faccb7dcd20
Jidong Xiao <jidong.xiao@gmail.com>
2022-09-14 17:25:59 -0600

    make it fancy, like, really fancy

--- buddy/buddy.js
+++ buddy/buddy.js
@@ -1,13 +1,17 @@
 // first we need to create a stage
 var stage = new Konva.Stage({
     container: 'container',   // id of container <div>
-    width: 2000,
-    height: 2000
+    width: 1500,
+    height: 1100,
 });
 
 // then create layer
 var layer = new Konva.Layer();
 
+// add the layer to the stage
+stage.add(layer);
+
+// we use these two arrows on the doubly linked list to connect nodes.
 function drawTwoArrows(offset,id) {
 	let arrowright = new Konva.Arrow({
     	points: [980, 65+offset, 1045, 65+offset],
@@ -78,7 +82,7 @@ var yStart1=50;
 
 var base = new Konva.Text({
     x: xStart1,
-    y: 200,
+    y: 5,
     id:'base',
     text: "base",
     fontSize: 28,
@@ -100,7 +104,8 @@ var p = new Konva.Text({
     x: xStart1,
     y: 300,
     id:'p',
-    text: "p (malloc(1) returns p)",
+    text: "p (let p point to the block header we just found)",
+    //text: "p (malloc(1) returns p)",
     fontSize: 28,
     fontFamily: 'Calibri',
     fill: '#555',
@@ -109,6 +114,240 @@ var p = new Konva.Text({
 // the large memory chunk
 drawRectWithText(xStart1, yStart1, cellWidth1, cellHeight, "", "#82fff7","hpid");
 
+// draw the first block header, this should be there in the initial state: i.e., it's created in buddy_init().
+// drawRectWithText(xStart1, yStart1, 10, cellHeight, "", "#cd5c5c","bh9");
+
+var txtbh9 = new Konva.Text({
+        id: "txt:bh9",
+        x:  20,
+        y: 120,
+        text: "",
+        fontSize: 24,
+        fontFamily: 'Calibri',
+        fill: 'black',
+        align: 'center',
+        padding: 10,
+});
+
+var recbh9 = new Konva.Rect({
+        id: "rec:bh9",
+        x: xStart1,
+        y: yStart1,
+        stroke: '#555',
+        strokeWidth: 2,
+        fill: '#cd5c5c',
+        width: 10,
+        height: cellHeight,
+        cornerRadius: 0,
+});
+
+layer.add(txtbh9);
+layer.add(recbh9);
+
+recbh9.on('mouseover', function(){
+	/* when the mouse is over the block header, we want the corresponding block header node on the linked list to be highlighted. */
+	stage.find('#rec:node9').fill('#33ffc4');
+	if(pc<16){
+		txtbh9.text('tag: FREE, kval: 9');
+		//this.fill('black');
+	}else if(pc<17){
+		/* malloc(1) returns. */
+		txtbh9.text('tag: RESERVED, kval: 5');
+	}else if(pc<22){
+		/* the user calls free(). */
+		txtbh9.text('tag: FREE, kval: 5');
+	}else if(pc<25){
+		/* coalesced with level 5 buddy. */
+		txtbh9.text('tag: FREE, kval: 6');
+	}else if(pc<28){
+		/* coalesced with level 6 buddy. */
+		txtbh9.text('tag: FREE, kval: 7');
+	}else if(pc<=30){
+		/* coalesced with level 7 buddy. */
+		txtbh9.text('tag: FREE, kval: 8');
+	}else if(pc>30){
+		/* coalesced with level 8 buddy. */
+		txtbh9.text('tag: FREE, kval: 9');
+	}
+	layer.draw();
+});
+
+recbh9.on('mouseout', function(){
+	//this.fill('#cd5c5c');
+	// show nothing, once the mouse leaves.
+	txtbh9.text('');
+	// restore to its original color.
+	stage.find('#rec:node9').fill('#99dd');
+	layer.draw();
+});
+
+/* repeat the same logic for block header 8. */
+var txtbh8 = new Konva.Text({
+        id: "txt:bh8",
+        x:  220,
+        y: 120,
+        text: "",
+        fontSize: 24,
+        fontFamily: 'Calibri',
+        fill: 'black',
+        align: 'center',
+        padding: 10,
+});
+
+var recbh8 = new Konva.Rect({
+        id: "rec:bh8",
+        x: xStart1+cellWidth1*0.5,
+        y: yStart1,
+        stroke: '#555',
+        strokeWidth: 2,
+        fill: '#cd5c5c',
+        width: 10,
+        height: cellHeight,
+        cornerRadius: 0,
+});
+
+recbh8.on('mouseover', function(){
+	txtbh8.text('tag: FREE, kval: 8');
+	/* when the mouse is over the block header, we want the corresponding block header node on the linked list to be highlighted. */
+	stage.find('#rec:node8').fill('#33ffc4');
+	layer.draw();
+});
+
+recbh8.on('mouseout', function(){
+	// show nothing, once the mouse leaves.
+	txtbh8.text('');
+	// restore to its original color.
+	stage.find('#rec:node8').fill('#99dd');
+	layer.draw();
+});
+
+/* repeat the same logic for block header 7. */
+var txtbh7 = new Konva.Text({
+        id: "txt:bh7",
+        x:  120,
+        y: 120,
+        text: "",
+        fontSize: 24,
+        fontFamily: 'Calibri',
+        fill: 'black',
+        align: 'center',
+        padding: 10,
+});
+
+var recbh7 = new Konva.Rect({
+        id: "rec:bh7",
+        x: xStart1+cellWidth1*0.25,
+        y: yStart1,
+        stroke: '#555',
+        strokeWidth: 2,
+        fill: '#cd5c5c',
+        width: 10,
+        height: cellHeight,
+        cornerRadius: 0,
+});
+
+recbh7.on('mouseover', function(){
+	txtbh7.text('tag: FREE, kval: 7');
+	/* when the mouse is over the block header, we want the corresponding block header node on the linked list to be highlighted. */
+	stage.find('#rec:node7').fill('#33ffc4');
+	layer.draw();
+});
+
+recbh7.on('mouseout', function(){
+	// show nothing, once the mouse leaves.
+	txtbh7.text('');
+	// restore to its original color.
+	stage.find('#rec:node7').fill('#99dd');
+	layer.draw();
+});
+
+/* repeat the same logic for block header 6. */
+var txtbh6 = new Konva.Text({
+        id: "txt:bh6",
+        x:  60,
+        y: 120,
+        text: "",
+        fontSize: 24,
+        fontFamily: 'Calibri',
+        fill: 'black',
+        align: 'center',
+        padding: 10,
+});
+
+var recbh6 = new Konva.Rect({
+        id: "rec:bh6",
+        x: xStart1+cellWidth1*0.125,
+        y: yStart1,
+        stroke: '#555',
+        strokeWidth: 2,
+        fill: '#cd5c5c',
+        width: 10,
+        height: cellHeight,
+        cornerRadius: 0,
+});
+
+recbh6.on('mouseover', function(){
+	txtbh6.text('tag: FREE, kval: 6');
+	/* when the mouse is over the block header, we want the corresponding block header node on the linked list to be highlighted. */
+	stage.find('#rec:node6').fill('#33ffc4');
+	layer.draw();
+});
+
+recbh6.on('mouseout', function(){
+	// show nothing, once the mouse leaves.
+	txtbh6.text('');
+	// restore to its original color.
+	stage.find('#rec:node6').fill('#99dd');
+	layer.draw();
+});
+
+/* repeat the same logic for block header 5. */
+var txtbh5 = new Konva.Text({
+        id: "txt:bh5",
+        x:  30,
+        y: 120,
+        text: "",
+        fontSize: 24,
+        fontFamily: 'Calibri',
+        fill: 'black',
+        align: 'center',
+        padding: 10,
+});
+
+var recbh5 = new Konva.Rect({
+        id: "rec:bh5",
+        x: xStart1+cellWidth1*0.0625,
+        y: yStart1,
+        stroke: '#555',
+        strokeWidth: 2,
+        fill: '#cd5c5c',
+        width: 10,
+        height: cellHeight,
+        cornerRadius: 0,
+});
+
+recbh5.on('mouseover', function(){
+	txtbh5.text('tag: FREE, kval: 5');
+	/* when the mouse is over the block header, we want the corresponding block header node on the linked list to be highlighted. */
+	stage.find('#rec:node5').fill('#33ffc4');
+	layer.draw();
+});
+
+recbh5.on('mouseout', function(){
+	// show nothing, once the mouse leaves.
+	txtbh5.text('');
+	// restore to its original color.
+	stage.find('#rec:node5').fill('#99dd');
+	layer.draw();
+});
+
+var cellWidth=160;
+var xStart=1120;
+var yStart=50;
+
+var cellWidth=160;
+var xStart=1120;
+var yStart=50;
 
 var cellWidth=160;
 var xStart=1120;
@@ -142,17 +381,17 @@ drawRectWithText(xStart, yStart+400, cellWidth, cellHeight, "avail[1] (tag=UNUSE
 drawRectWithText(xStart, yStart+500, cellWidth, cellHeight, "avail[0] (tag=UNUSED, kval=0)", "#69dd","list 0");
 
 var arrowBase = new Konva.Arrow({
-    points: [10, 195, 10, 100],
-    pointerLength: 10,
-    pointerWidth: 10,
+    points: [10, 30, 10, 50],
+    pointerLength: 5,
+    pointerWidth: 5,
     id: "arrowbase",
-    fill: 'orange',
-    stroke: 'orange',
+    fill: '#ff00ff',
+    stroke: '#ff00ff',
     strokeWidth: 5,
 });
 
 var arrowP = new Konva.Arrow({
-    points: [20, 295, 20, 100],
+    points: [10, 295, 10, 100],
     pointerLength: 10,
     pointerWidth: 10,
     id: "arrowp",
@@ -421,9 +660,6 @@ var arrowSearch = new Konva.Arrow({
     strokeWidth: 5,
 });
 
-// add the layer to the stage
-stage.add(layer);
-
 layer.add(base);
 layer.add(size);
 layer.add(headers);
@@ -517,13 +753,11 @@ var arrowarraynext5 = [980, 465, 850, 420, 680, 465];
 var arrowarrayprev5 = [680, 485, 630, 490, 680, 500];
 
 var arrowarraybuddy = [10+cellWidth1*0.5, 245, 10+cellWidth1*0.5, 100];
-var arrowarrayp = [20, 295, 20, 100];
+var arrowarrayp = [10, 295, 10, 100];
 
 /* this runs when user clicks the "next step" button */
 function nextstep() {
     if(pc == 1){
-		// draw the first block header
-		drawRectWithText(xStart1, yStart1, 10, cellHeight, "", "#cd5c5c","blockheader");
 		layer.add(search);
 		layer.add(arrowSearch);
         layer.draw();
@@ -545,14 +779,24 @@ function nextstep() {
         layer.draw();
         pc=pc+1;
     }else if(pc == 6){
-		/* draw another block header. */
-		drawRectWithText(xStart1+cellWidth1*0.5, yStart1, 10, cellHeight, "", "#cd5c5c","bh8");
+		layer.add(arrowP);
+		layer.add(p);
+        layer.draw();
+        pc=pc+1;
+    }else if(pc == 7){
+		/* once p is already displayed, no need to show the additional sentence. */
+    	stage.find('#p').text("p");
+		/* draw block header 8. */
+		//drawRectWithText(xStart1+cellWidth1*0.5, yStart1, 10, cellHeight, "", "#cd5c5c","bh8");
+		layer.add(txtbh8);
+		layer.add(recbh8);
+
 		layer.add(arrowBuddy);
 		layer.add(buddy);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 7){
-		/* draw another block header. */
+    }else if(pc == 8){
+		/* draw another block header on the linked list. */
     	stage.find('#txt:node9').hide();
     	stage.find('#rec:node9').hide();
 		drawRectWithText(1045, yStart-300, cellWidth, cellHeight, "block header (tag=FREE, kval=8)", "#99dd","node8");
@@ -584,16 +828,19 @@ function nextstep() {
     	stage.find('#search').x(350);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 8){
-		/* draw another block header. */
-		drawRectWithText(xStart1+cellWidth1*0.25, yStart1, 10, cellHeight, "", "#cd5c5c","bh7");
+    }else if(pc == 9){
+		/* draw block header 7. */
+		//drawRectWithText(xStart1+cellWidth1*0.25, yStart1, 10, cellHeight, "", "#cd5c5c","bh7");
+		layer.add(txtbh7);
+		layer.add(recbh7);
+
 		arrowarraybuddy[0] = xStart1+cellWidth1*0.25;
 		arrowarraybuddy[2] = xStart1+cellWidth1*0.25;
     	stage.find('#arrowbuddy').points(arrowarraybuddy);
     	stage.find('#buddy').x(xStart1+cellWidth1*0.25);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 9){
+    }else if(pc == 10){
 		drawRectWithText(1045, yStart-200, cellWidth, cellHeight, "block header (tag=FREE, kval=7)", "#99dd","node7");
 		arrowarraynext7[0] = arrowarraynext7[0] + 365;
 		arrowarraynext7[2] = arrowarraynext7[2] + 200;
@@ -608,16 +855,19 @@ function nextstep() {
 		movearrowDownsplit();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 10){
-		/* draw another block header. */
-		drawRectWithText(xStart1+cellWidth1*0.125, yStart1, 10, cellHeight, "", "#cd5c5c","bh6");
+    }else if(pc == 11){
+		/* draw block header 6. */
+		//drawRectWithText(xStart1+cellWidth1*0.125, yStart1, 10, cellHeight, "", "#cd5c5c","bh6");
+		layer.add(txtbh6);
+		layer.add(recbh6);
+
 		arrowarraybuddy[0] = xStart1+cellWidth1*0.125;
 		arrowarraybuddy[2] = xStart1+cellWidth1*0.125;
     	stage.find('#arrowbuddy').points(arrowarraybuddy);
     	stage.find('#buddy').x(xStart1+cellWidth1*0.125);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 11){
+    }else if(pc == 12){
 		drawRectWithText(1045, yStart-100, cellWidth, cellHeight, "block header (tag=FREE, kval=6)", "#99dd","node6");
 		arrowarraynext6[0] = arrowarraynext6[0] + 365;
 		arrowarraynext6[2] = arrowarraynext6[2] + 200;
@@ -632,16 +882,19 @@ function nextstep() {
 		movearrowDownsplit();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 12){
-		/* draw another block header. */
-		drawRectWithText(xStart1+cellWidth1*0.0625, yStart1, 10, cellHeight, "", "#cd5c5c","bh5");
+    }else if(pc == 13){
+		/* draw block header 5. */
+		//drawRectWithText(xStart1+cellWidth1*0.0625, yStart1, 10, cellHeight, "", "#cd5c5c","bh5");
+		layer.add(txtbh5);
+		layer.add(recbh5);
+
 		arrowarraybuddy[0] = xStart1+cellWidth1*0.0625;
 		arrowarraybuddy[2] = xStart1+cellWidth1*0.0625;
     	stage.find('#arrowbuddy').points(arrowarraybuddy);
     	stage.find('#buddy').x(xStart1+cellWidth1*0.0625);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 13){
+    }else if(pc == 14){
 		drawRectWithText(1045, yStart, cellWidth, cellHeight, "block header (tag=FREE, kval=5)", "#99dd","node5");
 		arrowarraynext5[0] = arrowarraynext5[0] + 365;
 		arrowarraynext5[2] = arrowarraynext5[2] + 200;
@@ -656,9 +909,15 @@ function nextstep() {
 		movearrowDownsplit();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 14){
-		layer.add(arrowP);
-		layer.add(p);
+    }else if(pc == 15){
+		/* move p 24 bytes forward is the first step in free. */
+		arrowarrayp[0] = arrowarrayp[0] + 10;
+		arrowarrayp[2] = arrowarrayp[2] + 10;
+    	stage.find('#arrowp').points(arrowarrayp);
+    	stage.find('#p').text("p (move p 24 bytes forward, and malloc(1) returns p)");
+		/* hide buddy, don't need it for now. */
+    	stage.find('#arrowbuddy').hide();
+    	stage.find('#buddy').hide();
         layer.draw();
         pc=pc+1;
 	}
@@ -666,13 +925,13 @@ function nextstep() {
 
 /* this runs when user clicks the "next step in free()" button */
 function nextstepinfree() {
-    if(pc == 15){
+    if(pc == 16){
     	stage.find('#p').text("p (now the user calls free(p))");
     	stage.find('#search').hide();
     	stage.find('#arrowsearch').hide();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 16){
+    }else if(pc == 17){
 		/* move p back 24 bytes is the first step in free. */
 		arrowarrayp[0] = arrowarrayp[0] - 10;
 		arrowarrayp[2] = arrowarrayp[2] - 10;
@@ -680,7 +939,13 @@ function nextstepinfree() {
     	stage.find('#p').text("p (move p back 24 bytes)");
         layer.draw();
         pc=pc+1;
-    }else if(pc == 17){
+    }else if(pc == 18){
+		/* show buddy, find our first buddy in free(). */
+    	stage.find('#arrowbuddy').show();
+    	stage.find('#buddy').show();
+        layer.draw();
+        pc=pc+1;
+    }else if(pc == 19){
     	stage.find('#search').text("remove buddy from list");
     	stage.find('#search').x(1050);
     	stage.find('#search').y(620);
@@ -694,7 +959,7 @@ function nextstepinfree() {
     	stage.find('#p').text("p");
         layer.draw();
         pc=pc+1;
-    }else if(pc == 18){
+    }else if(pc == 20){
 		/* the actual removal of buddy */
     	stage.find('#rec:node5').hide();
     	stage.find('#txt:node5').hide();
@@ -713,7 +978,7 @@ function nextstepinfree() {
     	stage.find('#arrowprev5').points(arrowarrayprev5);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 19){
+    }else if(pc == 21){
 		/* update buddy, hide the old one, don't need it anymore. */
 		arrowarraybuddy[0] = xStart1+cellWidth1*0.125;
 		arrowarraybuddy[2] = xStart1+cellWidth1*0.125;
@@ -724,7 +989,7 @@ function nextstepinfree() {
     	stage.find('#txt:bh5').hide();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 20){
+    }else if(pc == 22){
 		/* move 100 up, 620 to 520 */
     	stage.find('#search').y(520);
 		arrowarray1[1] = arrowarray1[1] - 100;
@@ -734,7 +999,7 @@ function nextstepinfree() {
     	stage.find('#arrowsearch').show();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 21){
+    }else if(pc == 23){
 		/* the actual removal of buddy */
     	stage.find('#rec:node6').hide();
     	stage.find('#txt:node6').hide();
@@ -753,7 +1018,7 @@ function nextstepinfree() {
     	stage.find('#arrowprev6').points(arrowarrayprev6);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 22){
+    }else if(pc == 24){
 		/* update buddy, hide the old one, don't need it anymore. */
 		arrowarraybuddy[0] = xStart1+cellWidth1*0.25;
 		arrowarraybuddy[2] = xStart1+cellWidth1*0.25;
@@ -764,7 +1029,7 @@ function nextstepinfree() {
     	stage.find('#txt:bh6').hide();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 23){
+    }else if(pc == 25){
 		/* move 100 up, 520 to 420 */
     	stage.find('#search').y(420);
 		arrowarray1[1] = arrowarray1[1] - 100;
@@ -774,7 +1039,7 @@ function nextstepinfree() {
     	stage.find('#arrowsearch').show();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 24){
+    }else if(pc == 26){
 		/* the actual removal of buddy */
     	stage.find('#rec:node7').hide();
     	stage.find('#txt:node7').hide();
@@ -793,7 +1058,7 @@ function nextstepinfree() {
     	stage.find('#arrowprev7').points(arrowarrayprev7);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 25){
+    }else if(pc == 27){
 		/* update buddy, hide the old one, don't need it anymore. */
 		arrowarraybuddy[0] = xStart1+cellWidth1*0.5;
 		arrowarraybuddy[2] = xStart1+cellWidth1*0.5;
@@ -804,7 +1069,7 @@ function nextstepinfree() {
     	stage.find('#txt:bh7').hide();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 26){
+    }else if(pc == 28){
 		/* move 100 up, 420 to 320 */
     	stage.find('#search').y(320);
 		arrowarray1[1] = arrowarray1[1] - 100;
@@ -814,7 +1079,7 @@ function nextstepinfree() {
     	stage.find('#arrowsearch').show();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 27){
+    }else if(pc == 29){
 		/* the actual removal of buddy */
     	stage.find('#rec:node8').hide();
     	stage.find('#txt:node8').hide();
@@ -833,7 +1098,7 @@ function nextstepinfree() {
     	stage.find('#arrowprev8').points(arrowarrayprev8);
         layer.draw();
         pc=pc+1;
-    }else if(pc == 28){
+    }else if(pc == 30){
 		/* update buddy, hide the old one, don't need it anymore. */
     	stage.find('#arrowbuddy').hide();
     	stage.find('#buddy').hide();
@@ -842,7 +1107,7 @@ function nextstepinfree() {
     	stage.find('#txt:bh8').hide();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 29){
+    }else if(pc == 31){
 		/* move 100 up, 320 to 220 */
     	stage.find('#search').y(220);
     	stage.find('#search').text("    add one node here");
@@ -853,7 +1118,7 @@ function nextstepinfree() {
     	stage.find('#arrowsearch').show();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 30){
+    }else if(pc == 32){
 		/* restore everything at level 9 to its initial state. */
     	stage.find('#txt:node9').show();
     	stage.find('#rec:node9').show();
@@ -872,11 +1137,11 @@ function nextstepinfree() {
     	stage.find('#arrowsearch').hide();
         layer.draw();
         pc=pc+1;
-    }else if(pc == 31){
+    }else if(pc == 33){
     	stage.find('#p').text("p is now freed");
         layer.draw();
         pc=pc+1;
-    }else if(pc == 32){
+    }else if(pc == 34){
     	stage.find('#p').hide();
     	stage.find('#arrowp').hide();
         layer.draw();
